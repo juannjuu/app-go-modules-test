5e6e7a5a406a85d59c0b8f4fa4523cd394280e2b
Juan Rahmadhika <juannjuu@Juans-MacBook-Pro.local>
2023-05-16 16:08:47 +0700

    use go-modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module juannjuu/app-go-modules-test/v2
+
+go 1.20
+
+require github.com/juannjuu/go-module-test/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_modules "github.com/juannjuu/go-module-test/v2"
+)
+
+func main() {
+	fmt.Println(go_modules.SayHello("Juan"))
+}
